7acac4220f167a58f05156fefe3959d469cf1527
ejbullock <e.j.bullock@outlook.com>
2022-10-24 23:40:45 -0400

    Added validation for Date format

--- src/main/java/controller/util/InputValidation.java
+++ src/main/java/controller/util/InputValidation.java
@@ -2,6 +2,13 @@ package controller.util;
 
 import mockDB.ToolDataMapper;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+
 public class InputValidation {
 
     public static void validateDiscountPercent(int discountPercent) throws Exception{
@@ -22,6 +29,29 @@ public class InputValidation {
             mapper.readToolInfo(toolCode);
         }catch(NullPointerException e){
             throw new Exception("Please enter a valid 4 character tool code.");
+        }
+    }
+
+    public static void isValidDateFormat(String value) throws Exception{
+        Locale locale = Locale.ENGLISH;
+        LocalDateTime ldt = null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy", locale);
+
+        try {
+            ldt = LocalDateTime.parse(value, formatter);
+            String result = ldt.format(formatter);
+        } catch (DateTimeParseException e) {
+            try {
+                LocalDate ld = LocalDate.parse(value, formatter);
+                String result = ld.format(formatter);
+            } catch (DateTimeParseException exp) {
+                try {
+                    LocalTime lt = LocalTime.parse(value, formatter);
+                    String result = lt.format(formatter);
+                } catch (DateTimeParseException e2) {
+                    throw new Exception("Please enter a valid date in the format MM/dd/yyyy.");
+                }
+            }
         }
     }
 
